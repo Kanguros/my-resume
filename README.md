8ac3520f93d32dfe1894472810177688bc1f11d9
Kangur <urbanek.kamil@gmail.com>
2022-05-31 00:44:59 +0200

    Trying to PDF

--- README.md
+++ README.md
@@ -21,9 +21,37 @@ npm install
 
 - `content` - Data for resume.
 - `theme` - Theme for `jsonresume`. It is a copy/paste of default one. 
-- `invoke.yaml`, `tasks.py` - Tasks and config for `invoke`.
+- `invoke.yaml`, `tasks.py` - Config and Tasks for `invoke`.
+
+
+### Export to PDF
+
+In order to export resume to PDF format, `s` is needed.
+
+1. Install
+```commandline
+$wkh_url = "https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox-0.12.6-1.msvc2015-win64.exe"
+$fn = "wkh.exe"
+
+Invoke-WebRequest -Uri $wkh -OutFile $fn
+Invoke-Item $fn
+```
+
+3. Add `` binary folder path to system's `PATH`. By default, it is located in `C:\Program Files\wkhtmltopdf\bin`
+Open cmd or PowerShell as Administrator and paste:
+```commandline
+setx /M path "%path%;C:\Program Files\wkhtmltopdf\bin"
+```
 
 ## Usage
+
+```commandline
+invoke 
+```
+
+## Development
+
+
 
 ```commandline
  resume export resume.html --theme .\theme_even\
--- tasks.py
+++ tasks.py
@@ -1,7 +1,8 @@
 import json
 
 import yaml
-from invoke import task
+from invoke import task, Failure
+import pdfkit
 
 
 def p(s, prefix="|> "):
@@ -20,7 +21,7 @@ def save_json(data, file):
 
 
 @task
-def parse_yaml(c):
+def parse(c):
     """Parse content of a resume from `resume.yaml` to `resume.json."""
     input_file = f"{c.filename}.yaml"
     input_path = f"{c.content_folder}\\{input_file}"
@@ -37,14 +38,49 @@ def parse_yaml(c):
     p(f"Data saved in [{json_path}]")
 
 
-@task(parse_yaml)
-def export(c):
+@task(parse)
+def html(c):
     """Generate resume.html."""
-    html_file = f"{c.filename}.html"
     resume_path = f"{c.content_folder}\\{c.filename}.json"
-    theme = c.theme_folder
-    p(f"Exporting [{resume_path}] with theme [{theme}] to [{html_file}]")
+    html_file = f"{c.content_folder}\\{c.filename}.html"
 
-    cmd = f"resume export {html_file} --resume {resume_path} --theme {theme}"
+    p(f"Exporting [{resume_path}] with theme [{c.theme_folder}] to [{html_file}]")
+
+    cmd = f"resume export {html_file} --resume {resume_path} --theme {c.theme_folder}"
     p(f"Executing command [{cmd}]")
-    c.run(cmd)
+    output = c.run(cmd)
+    if output.stderr:
+        raise Failure(f"FAILED. Export failed due to: {output['stderr']}")
+    else:
+        p(f"Resume exported as [{html_file}]")
+
+
+@task(html)
+def pdf(c):
+    PDF_OPTIONS = {
+        'enable-local-file-access': True,
+        'page-size': 'A4',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
+        'margin-bottom': '0.75in',
+        'margin-left': '0.75in',
+        'encoding': "UTF-8"
+    }
+
+    html_file = f"{c.content_folder}\\{c.filename}.html"
+    pdf_file = f"{c.filename}.pdf"
+    p(f"Exporting [{html_file}] to [{pdf_file}]")
+    html_file = html_file.replace(r".\"", '')
+
+    with open(html_file, 'r') as f:
+        result = pdfkit.from_file(f, pdf_file,
+                                  options=PDF_OPTIONS,
+                                  verbose=True)
+
+    if not result:
+        raise Failure(f"FAILED. Export failed due to: {result}")
+    else:
+        p(f"Resume exported as [{pdf_file}]")
+    #
+    # pdfkit.from_url("https://google.com", pdf_file,
+    #                  options=PDF_OPTIONS)
